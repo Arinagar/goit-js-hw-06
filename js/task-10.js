c3a18d6048b67951a5698e96f2819e37d916f329
Mark Shliakhov <valmarksh@gmail.com>
2022-11-07 14:27:40 +0200

    finish 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -5,8 +5,6 @@ function getRandomHexColor() {
 const divEl = document.querySelector('#boxes');
 const createEl = document.querySelector('button[data-create]');
 const destroyEl = document.querySelector('button[data-destroy]');
-// let inputEl = document.getElementsByTagName('input')[0].value;
-// let inputEl = document.querySelector('input[type="number"]').value;
 const inputEl = document.querySelector('input[type="number"]');
 
 let newInput = 0;
@@ -14,7 +12,7 @@ let newInput = 0;
 function onBoxesCreateClick(amount) {
   let initialSize = 30;
 
-  for (let i = 0; i < amount; i += 1) {
+  for (let i = 0; i < Number(inputEl.value); i += 1) {
     const newBox = document.createElement('div');
     newBox.style.backgroundColor = getRandomHexColor();
     initialSize += 10;
@@ -29,18 +27,16 @@ function onBoxesDestroyClick() {
   divEl.innerHTML = '';
 }
 
-function checkInput() {
-  newInput = inputEl.value;
-}
-
 createEl.addEventListener(
   'click',
-  function () {
-    onBoxesCreateClick(newInput);
-  },
-  false
-);
-destroyEl.addEventListener('click', onBoxesDestroyClick);
-inputEl.addEventListener('input', checkInput);
 
-// как сделать без доп ивента по отслеживанию инпута???
+  onBoxesCreateClick
+);
+
+destroyEl.addEventListener('click', onBoxesDestroyClick);
+
+//  Более длинный вариант с отслеживанием инпута
+// function checkInput() {
+//   newInput = inputEl.value;
+// }
+// inputEl.addEventListener('input', checkInput);
